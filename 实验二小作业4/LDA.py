6032d53fa5df64d7e735a8171578848c9c75da83
Wonghiu <wonghiu45@163.com>
2017-12-28 10:09:11 +0800

    LDA修改
    1.解决了图像显示不正确的问题
    2.将byes分界曲线更平滑

--- 实验二小作业4/LDA.py
+++ 实验二小作业4/LDA.py
@@ -6,7 +6,9 @@ import operator
 import matplotlib
 import matplotlib.pyplot as plt
 import sympy
-#袁俊一签到
+import byesclassify
+
+
 
 def getdata(filename):
     f  = open(filename)
@@ -21,16 +23,55 @@ def getdata(filename):
     return mansls,manslabels
 # end of createDataSet
 
-# 画图
-def draw(w, mean1,mean2,group1, group2):
+
+def drawcompare(w, mean1,mean2,group1, group2,rawboy,rawgirl,xianyan1=0.5,xianyan2=0.5):
+    x3 = []
+    y3 = []
+
+    for i in np.arange(100, 200,0.1):
+        for j in range(30, 100):
+            prediction = byesclassify.duoweiclassifier([i, j], rawboy,rawgirl, xianyan1, xianyan2)
+            if 0.500 == round(prediction, 3):
+                x3.append(i)
+                y3.append(j)
     plt.figure(1)
     ax = plt.subplot(111)
+    # ax.plot(range(0,200),range(0,200))
+    plt.xlim(140, 200)
+    plt.ylim(35, 100)
     ax.scatter(np.array(group1)[0], np.array(group1)[1], s=30, c='red', marker='s')  # 训练数据散点图
     ax.scatter(np.array(group2)[0], np.array(group2)[1], s=30, c='green')
     w = np.array(w)
     x = arange(100, 200, 1)
-    y = array((-w[0] * x) / w[1])  # 投影直线
-    y1 = array(w[1] * (x - 0.5 * (array(mean1)[0, 0] + array(mean2)[0, 0])) / (w[0]) + 0.5 * (
+
+    y = array((w[1] * x) / w[0])  # 投影直线
+
+    y1 = array(-w[0] * (x - 0.5 * (array(mean1)[0, 0] + array(mean2)[0, 0])) / (w[1]) + 0.5 * (
+    array(mean1)[1, 0] + array(mean2)[1, 0]))  # 边界分割线
+    plt.figure(1)
+    ax1 = plt.subplot(111)
+    # plt.plot(x,y)
+    plt.plot(x, y1)
+    plt.plot(x, y)
+    plt.plot(x3,y3)
+    # plt.plot(x,y1)
+    plt.show()
+
+
+# 画图
+def draw(w, mean1,mean2,group1, group2):
+    plt.figure(1)
+    ax = plt.subplot(111)
+    #ax.plot(range(0,200),range(0,200))
+    plt.xlim(0,200,5)
+    plt.ylim(0,200,5)
+    ax.scatter(np.array(group1)[0], np.array(group1)[1], s=30, c='red', marker='s')  # 训练数据散点图
+    ax.scatter(np.array(group2)[0], np.array(group2)[1], s=30, c='green')
+    w = np.array(w)
+    x = arange(100, 200, 1)
+    y = array((w[1] * x) / w[0])  # 投影直线
+
+    y1 = array(-w[0] * (x - 0.5 * (array(mean1)[0, 0] + array(mean2)[0, 0])) / (w[1]) + 0.5 * (
     array(mean1)[1, 0] + array(mean2)[1, 0]))  # 边界分割线
     plt.figure(1)
     ax1 = plt.subplot(111)
@@ -40,6 +81,7 @@ def draw(w, mean1,mean2,group1, group2):
 
     # plt.plot(x,y)
     plt.plot(x, y1)
+    plt.plot(x,y)
     # plt.plot(x,y1)
     plt.show()
 
@@ -106,7 +148,7 @@ def predict(data, w, mean1, mean2):
 groupboy,l=getdata('boy.txt')
 groupgirl,ls =getdata('girl.txt')
 w,mean1,mean2,group1,group2= train(groupboy,groupgirl)
-draw(w,mean1,mean2,group1,group2)
+drawcompare(w,mean1,mean2,group1,group2,groupboy,groupgirl)
 
 #-------------------------------------------------------
 testboy,labels=getdata('boy82.txt')#女生标记为0，男生标记为1
@@ -120,13 +162,13 @@ for i in range(shape(group3)[1]):
         count = count+1
 
 print(count/shape(group3)[1])
-def liuyi(data1,data2,labels1,labels2):
-    alldata=data1+data2
-    alllabels=labels1+labels2
-    for i in range(shape(data1)[0]+shape(data2)[0]):
-        test = alldata[i,:]
-        train = data[0:i,:]+data[i+1,:]
-        w, mean1, mean2, group1, group2 = train(data1, data2)
+# def liuyi(data1,data2,labels1,labels2):
+#     alldata=data1+data2
+#     alllabels=labels1+labels2
+#     for i in range(shape(data1)[0]+shape(data2)[0]):
+#         test = alldata[i,:]
+#         train = data[0:i,:]+data[i+1,:]
+#         w, mean1, mean2, group1, group2 = train(data1, data2)
 
 
 
