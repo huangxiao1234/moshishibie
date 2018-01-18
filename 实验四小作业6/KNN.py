dbf20f9b5d26f70ef83731c6b2a45cf87f7725ec
Wonghiu <wonghiu45@163.com>
2018-01-18 09:55:06 +0800

    KNN修改（加了注释和分界面）

--- 实验四小作业6/KNN.py
+++ 实验四小作业6/KNN.py
@@ -1,14 +1,17 @@
 from numpy import *
 import operator
-from os import listdir
-import matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
 
-def createDataSet():
-    group  = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
-    return group,labels
 def classify0(inX, dataSet, labels, k):
+    '''
+    KNN算法实现
+    :param inX:
+    :param dataSet:
+    :param labels:
+    :param k:
+    :return:
+    '''
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
@@ -22,94 +25,87 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-def file2matrix(filename):
-    fr = open(filename)
-    arrayOLines = fr.readlines()
-    numberOfLines = len(arrayOLines)
-    returnMat = zeros((numberOfLines,3))
-    classLabelVector = []
-    index = 0
-    for line in arrayOLines:
-        line = line.strip()
-        listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
-        index += 1
-    return returnMat,classLabelVector
+def getdata_girl_and_man(manfile,girlfile):
+    '''
+    用来将男生文件和女生文件合起来
+    :param manfile: 男生文件名
+    :param girlfile: 女生文件名
+    :return: 合起来后的data,labels,列表
+    '''
+    f = open(manfile)
+    lines = f.readlines()
+    mansls = []
+    for line in lines:
+        a = line.split()
+        a = [float(i) for i in a]
+        mansls.append(a)
+    manslabels = [1] * np.shape(mansls)[0]
 
-def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
-    ranges  = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
-    m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals,(m,1))
-    normDataSet = normDataSet/tile(ranges,(m,1))
-    return normDataSet,ranges,minVals
+    f1 = open(girlfile)
+    lines1 = f1.readlines()
+    girlls = []
+    for line in lines1:
+        a1 = line.split()
+        a1 = [float(i) for i in a1]
+        girlls.append(a1)
+    girllabels = [0] * np.shape(girlls)[0]
+    manslabels.extend(girllabels)
+    alllabels = manslabels
+    mansls.extend(girlls)
+    alldatas = mansls
+    return alldatas,alllabels
 
-def datingClassTest():
-    hoRatio = 0.10
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
-    normMat,ranges,minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
-    errorCount = 0.0
-    for i in range(numTestVecs) :
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print("the classifier came back with:%d,the real answer is :%d"%(classifierResult,datingLabels[i]))
-        if (classifierResult!=datingLabels[i]):
-            errorCount +=1.0
-    print("the total error rate is :%f"%(errorCount/float(numTestVecs)))
 
-def classifyperson():
-    resultList = ['not at all','in small doses','in largedoses']
-    percentTats = float(raw_input)
+def getdata(filename1,classnumber):
+    '''
+    导入单个txt文件，将其转成列表
+    :param filename1: 文件名
+    :param classnumber: 将其判为哪一类，0or1
+    :return: 生成好的data,labels
+    '''
+    f  = open(filename1)
+    lines  = f.readlines()
+    mansls= []
+    for line in lines:
+        a=line.split()
+        a= [float(i) for i in a ]
+        mansls.append(a)
+    manslabels= [classnumber]*np.shape(mansls)[0]
+    return mansls,manslabels
 
-def img2vector(filename):
-    returnVect = zeros((1,1024))
-    fr = open(filename)
-    for i in range(32):
-        lineStr = fr.readline()
-        for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
-    return returnVect
+def draw(alldatas,k):
+    '''
+    做出KNN分界线
+    :param alldatas: 数据集
+    :param k: k近邻
+    :return:
+    '''
+    mantest = []  # 遍历每个点，将被判为男生的加入列表，最后作图就能得到分界面
+    girltest = []
 
-def handwritingClassTest():
-    hwLabels = []
-    trainingFileList = listdir('trainingDigits')
-    m = len(trainingFileList)
-    trainingMat = zeros((m,1024))
-    for i in range(m):
-        fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]
-        classNumStr = int(fileStr.split('_')[0])
-        hwLabels.append(classNumStr)
-        trainingMat[i,:] = img2vector('trainingDigits/%s'%fileNameStr)
-    testFileList = listdir('testDigits')
-    print(testFileList)
-    errorCount =0.0
-    mTest = len(testFileList)
-    for i in range(mTest):
-        fileNameStr = testFileList[i]
-        fileStr = fileNameStr.split('.')[0]
-        classNumStr = int (fileStr.split('_')[0])
-        vectorUnderTest = img2vector('testDigits/%s'%fileNameStr)
-        classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
-        print("the classifier came back with :%d ,the real answr is :%d"%(classifierResult,classNumStr))
-        if (classifierResult!=classNumStr):
-            errorCount +=1.0
-    print("\nthe total number of errors is: %d"%errorCount)
-    print("\nthe total error rate is: %f"%(errorCount/float(mTest)))
+    for i in np.arange(145, 185, 0.1):
+        for j in np.arange(30, 90, 0.1):
+            if classify0([i, j], alldatas[:, 0:2], alllabels, k) == 1:
+                mantest.append([i, j])
+            else:
+                girltest.append([i, j])
 
-# g,b = createDataSet()
-# c = classify0([0,0],g,b,3)
-# DataMat,Labels = file2matrix('datingTestSet2.txt')
+    ax = plt.subplot(111)
+    ax.scatter(np.array(mantest)[:, 0], np.array(mantest)[:, 1], s=10, c='tan', marker='s')
+    ax.scatter(np.array(girltest)[:, 0], np.array(girltest)[:, 1], s=10, c='silver', marker='s')
+    ax.scatter(np.array(mansls)[:, 0], np.array(mansls)[:, 1], s=30, c='red', marker='s')  # 训练数据散点图
+    ax.scatter(np.array(girlsls)[:, 0], np.array(girlsls)[:, 1], s=30, c='green', marker='s')
+    plt.show()
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# ax.scatter(DataMat[:,0],DataMat[:,1],30.0*array(Labels),30.0*array(Labels))
-# plt.show()
-# n,t,m = autoNorm(DataMat)
-# print(DataMat)
-#handwritingClassTest()
+
+mansls,manslabels=getdata('boy.txt',0)
+girlsls,girlslabels=getdata('girl.txt',1)
+
+manslabels.extend(girlslabels)
+alllabels = manslabels
+mansls.extend(girlsls)
+alldatas = np.array(mansls)
+draw(alldatas,4)
+
+
 
