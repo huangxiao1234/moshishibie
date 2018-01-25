c2db4a37dcc72bfc4f16ae0501a33e924709dc6b
Wonghiu <wonghiu45@163.com>
2018-01-25 10:11:16 +0800

    knn第一次修改
    将2维修改成了3维，并做出三维分界面

--- 实验四小作业6/KNN.py
+++ 实验四小作业6/KNN.py
@@ -2,6 +2,8 @@ from numpy import *
 import operator
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from tqdm import tqdm
 
 def classify0(inX, dataSet, labels, k):
     '''
@@ -12,6 +14,7 @@ def classify0(inX, dataSet, labels, k):
     :param k:
     :return:
     '''
+    result=k
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
@@ -23,7 +26,10 @@ def classify0(inX, dataSet, labels, k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+    # print(sortedClassCount)
+    if len(sortedClassCount)>1:
+        result=sortedClassCount[0][1]-sortedClassCount[1][1]
+    return sortedClassCount[0][0],result
 
 def getdata_girl_and_man(manfile,girlfile):
     '''
@@ -70,42 +76,46 @@ def getdata(filename1,classnumber):
         a=line.split()
         a= [float(i) for i in a ]
         mansls.append(a)
+
     manslabels= [classnumber]*np.shape(mansls)[0]
     return mansls,manslabels
 
-def draw(alldatas,k):
+def draw(alldatas,alllabels,mansls,girlsls,k):
     '''
     做出KNN分界线
+    只需要判断一个点被判为0或者1的次数是否相等就可以，相等就是分界面的一员
     :param alldatas: 数据集
     :param k: k近邻
     :return:
     '''
-    mantest = []  # 遍历每个点，将被判为男生的加入列表，最后作图就能得到分界面
-    girltest = []
+    fenjiemian=[]
 
-    for i in np.arange(145, 185, 0.1):
-        for j in np.arange(30, 90, 0.1):
-            if classify0([i, j], alldatas[:, 0:2], alllabels, k) == 1:
-                mantest.append([i, j])
-            else:
-                girltest.append([i, j])
+    for i in tqdm(np.arange(np.mean(alldatas[:,0])-30, np.mean(alldatas[:,0])+20, 6)):#步长放长，避免过拟合，分界面更平滑
+        for j in np.arange(np.mean(alldatas[:,1])-30, np.mean(alldatas[:,1])+20, 6):
+            for z in np.arange(np.mean(alldatas[:,2])-10, np.mean(alldatas[:,2])+10, 6):
+                class_result,class_sub=classify0([i, j, z], alldatas[:, 0:3], alllabels, k)
+                if class_sub==0 :
+                    fenjiemian.append([i, j,z])
 
-    ax = plt.subplot(111)
-    ax.scatter(np.array(mantest)[:, 0], np.array(mantest)[:, 1], s=10, c='tan', marker='s')
-    ax.scatter(np.array(girltest)[:, 0], np.array(girltest)[:, 1], s=10, c='silver', marker='s')
-    ax.scatter(np.array(mansls)[:, 0], np.array(mansls)[:, 1], s=30, c='red', marker='s')  # 训练数据散点图
-    ax.scatter(np.array(girlsls)[:, 0], np.array(girlsls)[:, 1], s=30, c='green', marker='s')
+    ax = plt.subplot(111,projection='3d')
+    ax.plot_trisurf(np.array(fenjiemian)[:, 0], np.array(fenjiemian)[:, 1],np.array(fenjiemian)[:, 2])#画出分界面
+    ax.scatter(np.array(mansls)[:, 0], np.array(mansls)[:, 1], np.array(mansls)[:, 2], c='red')
+    ax.scatter(np.array(girlsls)[:, 0], np.array(girlsls)[:, 1], np.array(girlsls)[:, 2], c='green')
     plt.show()
 
+#
+mansls1,manslabels1=getdata('boy.txt',0)
+girlsls1,girlslabels1=getdata('girl.txt',1)
+m=mansls1.copy()
+g=girlsls1.copy()
+manslabels1.extend(girlslabels1)
+alllabels = manslabels1
+mansls1.extend(girlsls1)
+alldatas = np.array(mansls1)
+draw(alldatas,alllabels,m,g,6)
+print(np.mean(alldatas[:,0]))
 
-mansls,manslabels=getdata('boy.txt',0)
-girlsls,girlslabels=getdata('girl.txt',1)
-
-manslabels.extend(girlslabels)
-alllabels = manslabels
-mansls.extend(girlsls)
-alldatas = np.array(mansls)
-draw(alldatas,4)
+# print(print('\n'.join([''.join([('LoveAndy'[(x-y)%8]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)])))
 
 
 
